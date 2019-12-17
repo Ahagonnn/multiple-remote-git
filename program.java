a881d5d14c4393cb88ca2165c4b4885a859df77f
felo <felonica.id@gmail.com>
2019-12-17 07:45:53 +0700

    Update program.java

--- program.java
+++ program.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 class program {
   public static void main(String[] args) {
     string kata;
-    Scanner inputUser = new Scanner(System.in);
+    Scanner inputUser;
+    inputUser = Scanner(System.in);
     System.out.print("Masukan kata yang ingin di print: ");
     kata = inputUser.next();
     System.out.println(kata);
